d7872e411a9cdb218189284d1a20127fad98e5b7
Nicolas Rossi <nick413rossi@gmail.com>
2023-02-15 20:29:05 -0500

    Fixed Use state update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import { Grid, Box, Button, FormControl, InputLabel, Select, MenuItem, List, ListItem, ListItemText, Divider } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { stateList } from './stateList'
 import axios from 'axios'
 import { DataGrid} from '@mui/x-data-grid'
@@ -22,12 +22,19 @@ export default function Home() {
   })
   const [placesData, setPlacesData] = useState([])
 
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  useEffect(() => {
+    retrieveParksInfo(stateChosen.id)
+    
+  }, [stateChosen])
+
   const updateStateSelect = (event) => {
     console.log(event.target.value)
     setState(event.target.value)
   }
 
-  const handleStateSubmit = () => {
+  const handleStateSubmit = async () => {
     console.log("Inside handleStateSubmit")
     for(let i = 0; i<stateList.length; i++) {
       if(state === stateList[i].id) {
@@ -38,20 +45,22 @@ export default function Home() {
         })
       }
     }
-    retrieveParksInfo(stateChosen.id)
+    // retrieveParksInfo(stateChosen.id)
   }
 
   const retrieveParksInfo = async (stateId) => {
-    await axios.get(`https://developer.nps.gov/api/v1/places?stateCode=` + stateId + `&api_key=eJnkCdoOGwmfjjCQTSLBaMugyccloNBRXKDj7kjq`).then(
+    let placesFixedOut
+    await axios.get(`https://developer.nps.gov/api/v1/places?stateCode=` + stateId + `&api_key=eJnkCdoOGwmfjjCQTSLBaMugyccloNBRXKDj7kjq&limit=10`).then(
       res => {
         const places = res.data
         let placesFixed = places.data
         console.log("Places Fixed: ", placesFixed)
 
-        setPlacesData(placesFixed)
+        placesFixedOut = placesFixed
       }
     )
-    console.log(placesData)
+    console.log(placesFixedOut)
+    setPlacesData(placesFixedOut)
   }
 
 
